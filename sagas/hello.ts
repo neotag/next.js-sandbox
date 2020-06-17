b0d1b5bb9ee85ba03024cc7573ae1324aa8d3be7
neotag <harada@gaji.jp>
2020-06-17 18:03:09 +0900

    Add Hello saga

---
+++ sagas/hello.ts
@@ -0,0 +1,25 @@
+import { all, call, fork, put, takeLatest } from 'redux-saga/effects';
+
+import { showHelloPage } from '../actions/hello';
+import * as Action from '../actions/helloConstants';
+import { getRandomMessage } from '../services/hello/randomMessage';
+
+function* runShowHelloPage(action: ReturnType<typeof showHelloPage.start>) {
+  const { sleep } = action.payload;
+
+  try {
+    const hello = yield call(getRandomMessage, sleep);
+
+    yield put(showHelloPage.succeed({ sleep }, { hello }));
+  } catch (error) {
+    yield put(showHelloPage.fail({ sleep }, error));
+  }
+}
+
+export function* watchShowHelloPage() {
+  yield takeLatest(Action.SHOW_HELLO_PAGE_START, runShowHelloPage);
+}
+
+export default function* rootSaga() {
+  yield all([fork(watchShowHelloPage)]);
+}
