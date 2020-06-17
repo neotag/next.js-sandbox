d9cf320fbea6a2035bb4f0511c95ea3732ffb480
neotag <harada@gaji.jp>
2020-06-17 15:54:32 +0900

    Add sample page

---
+++ pages/hello/index.tsx
@@ -0,0 +1,15 @@
+import React, { FC } from 'react';
+
+import Head from 'next/head';
+
+const HelloIndex: FC = () => (
+  <>
+    <Head>
+      <title>Hello World</title>
+    </Head>
+    <h1>Hello World index page</h1>
+    <p>こんにちわこんにちわ</p>
+  </>
+);
+
+export default HelloIndex;
