01c7c8f4d5852b95bd7f1426e4cf32c9301a65d4
neotag <harada@gaji.jp>
2020-06-17 18:07:02 +0900

    Connect state and dispatch to HelloIndex

--- pages/hello/index.tsx
+++ pages/hello/index.tsx
@@ -1,15 +1,64 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators, Dispatch } from 'redux';
 
 import Head from 'next/head';
+import { showHelloPage } from '../../actions/hello';
+import { HelloPageState } from '../../reducer';
+import { Hello } from '../../services/hello/models';
 
-const HelloIndex: FC = () => (
-  <>
-    <Head>
-      <title>Hello World</title>
-    </Head>
-    <h1>Hello World index page</h1>
-    <p>こんにちわこんにちわ</p>
-  </>
-);
+interface StateProps {
+  hello: Hello;
+  isLoading?: boolean;
+}
 
-export default HelloIndex;
+interface DispatchProps {
+  showHelloPageStart: (sleep: number) => void;
+}
+
+type EnhancedHelloPageProps = StateProps & DispatchProps;
+
+const mapStateToProps = (state: HelloPageState): StateProps => ({
+  hello: state.hello,
+  isLoading: state.isLoading,
+});
+
+const mapDispatchToProps = (dispatch: Dispatch): DispatchProps =>
+  bindActionCreators(
+    {
+      showHelloPageStart: (sleep: number) => showHelloPage.start({ sleep }),
+    },
+    dispatch,
+  );
+
+const HelloIndex: FC<EnhancedHelloPageProps> = ({
+  hello,
+  isLoading,
+  showHelloPageStart,
+}) => {
+  const [sleep, setSleep] = useState(0);
+
+  useEffect(() => {
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    const newSleep = Math.ceil(Math.random() * 10000);
+    setSleep(newSleep);
+    showHelloPageStart(newSleep);
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  return (
+    <>
+      <Head>
+        <title>Hello World</title>
+      </Head>
+      <h1>Hello World index page</h1>
+      <p>こんにちわこんにちわ</p>
+      {isLoading ? (
+        <p>Message is loading. waiting for {sleep}ms</p>
+      ) : (
+        <p>message: {hello.message}</p>
+      )}
+    </>
+  );
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(HelloIndex);
