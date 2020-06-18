1fa7cb73e61c2c67cb064f6e0d6445edcba441e8
neotag <harada@gaji.jp>
2020-06-18 21:50:51 +0900

    Add snapshot of HelloPage

--- __tests__/snapshot.tsx
+++ __tests__/snapshot.tsx
@@ -1,8 +1,30 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import renderer from 'react-test-renderer';
+import { createStore } from 'redux';
+
+import reducer from '../reducer';
+
+import HelloPage from '../pages/hello/index';
 import Index from '../pages/index';
 
-it('renders homepage unchanged', () => {
+it('renders Homepage unchanged', () => {
   const tree = renderer.create(<Index />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it('renders HelloPage unchanged with empty store', () => {
+  // Provider がないと HelloPage が render できないので空の store を食わせている
+  // store や saga task の変更などは検知しない
+  const emptyStore = createStore(reducer);
+
+  const tree = renderer
+    .create(
+      // tslint:disable-next-line: jsx-wrap-multiline
+      <Provider store={emptyStore}>
+        <HelloPage />
+      </Provider>,
+    )
+    .toJSON();
   expect(tree).toMatchSnapshot();
 });
