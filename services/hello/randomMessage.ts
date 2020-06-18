f12786b06228494629e4d9855d2d5bc3984cdfb4
neotag <harada@gaji.jp>
2020-06-18 18:59:14 +0900

    Able to getRandomMessage from test

--- services/hello/randomMessage.ts
+++ services/hello/randomMessage.ts
@@ -3,11 +3,11 @@
 // eslint-disable-next-line func-names
 
 export const messages: string[] = ['hi!', 'こんにちわ', '您好'];
+// tslint:disable-next-line: insecure-random
+export const createIndex = (sleep: number): number => sleep % messages.length;
 
 export const getRandomMessage = async (sleep = 3000): Promise<string> => {
-  // tslint:disable-next-line: insecure-random
-  const message = messages[Math.floor(Math.random() * messages.length)];
-  console.log('hoge', typeof message);
+  const message = messages[createIndex(sleep)];
   const createMessage = resolve => setTimeout(() => resolve(message), sleep);
 
   return new Promise(resolve => createMessage(resolve));
